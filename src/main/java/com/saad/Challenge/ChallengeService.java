d338b49b8dc67d66e52b3aca916280d5d4ed3311
Saad Sayyed <saadsyed950@gmail.com>
2024-10-17 08:01:14 +0530

    Took care of REST

--- src/main/java/com/saad/Challenge/ChallengeService.java
+++ src/main/java/com/saad/Challenge/ChallengeService.java
@@ -1,27 +1,31 @@
 package com.saad.Challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChallengeService {
-    private List<Challenge> challenges = new ArrayList<>();
+//    private List<Challenge> challenges = new ArrayList<>();
     private Long nextId = 1L;
+
+    @Autowired
+    ChallengeRepository challengeRepository;
 
     public ChallengeService() {
 
     }
 
     public List<Challenge> getAllChallenges() {
-        return challenges;
+        return challengeRepository.findAll();
     }
 
     public boolean addChallenges(Challenge challenge) {
         if(challenge != null) {
             challenge.setId(nextId++);
-            challenges.add(challenge);
+            challengeRepository.save(challenge);
             return true;
         } else  {
             return false;
@@ -29,26 +33,29 @@ public class ChallengeService {
     }
 
     public Challenge getAChallenge(String month) {
-        for (Challenge challenge : challenges) {
-            if(challenge.getMonth().equalsIgnoreCase(month)) {
-                return challenge;
-            }
-        }
-        return null;
+        Optional<Challenge> challenge = challengeRepository.findByMonthIgnoreCase(month);
+        return challenge.orElse(null);
     }
 
     public boolean updateChallenge(Long id, Challenge updatedChallenge) {
-        for (Challenge challenge : challenges) {
-            if(challenge.getId().equals(id)) {
-                challenge.setMonth(updatedChallenge.getMonth());
-                challenge.setDescription(updatedChallenge.getDescription());
-                return true;
-            }
+        Optional<Challenge> challenge = challengeRepository.findById(id);
+        if(challenge.isPresent()) {
+            Challenge updateToChallenge = challenge.get();
+            updateToChallenge.setMonth(updatedChallenge.getMonth());
+            updateToChallenge.setDescription(updateToChallenge.getDescription());
+            challengeRepository.save(updateToChallenge);
+            return true;
         }
+
         return false;
     }
 
     public boolean deletedChallenge(Long id) {
-        return challenges.removeIf(challenge -> challenge.getId().equals(id));
+        Optional<Challenge> challenge = challengeRepository.findById(id);
+        if(challenge.isPresent()) {
+            challengeRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
